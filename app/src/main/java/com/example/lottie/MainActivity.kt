213724e3bfd41a6341d67cf63fba9be31dfdcf38
OlegDmitrievich88 <dartdmitrich@gmail.com>
2023-01-23 23:05:03 +0300

    lottie

--- app/src/main/java/com/example/lottie/MainActivity.kt
+++ app/src/main/java/com/example/lottie/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.lottie
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import com.airbnb.lottie.Lottie
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val anim: LottieAnimationView = findViewById(R.id.lottie_anim)
+        anim.repeatCount = LottieDrawable.INFINITE
+        anim.repeatMode = LottieDrawable.REVERSE
+        anim.setAnimation(R.raw.lot)
+        anim.playAnimation()
 
 
     }
